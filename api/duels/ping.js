d5e5503fd9daad9a3ba6d1cbde3100c0c21cc113
eastercoder-dev <eastercoder@gmail.com>
2026-08-12 18:21:24 +0530

    Delete api directory

--- api/duels/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function handler(req, res) {
-    console.log("DUELS PING HIT");
-
-    return res.status(200).json({
-        success: true,
-        message: "Duels API is working"
-    });
-};
